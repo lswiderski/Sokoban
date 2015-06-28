769e4b0b173cb6607b59f00095539115d62b6c42
Neufrin <mail@lukaszswiderski.com>
2015-06-28 17:18:40 +0200

    Started working on levels

---
+++ Sokoban/SokobanPC/Block.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SokobanPC
+{
+    class Block
+    {
+        public bool IsEmpty { get; set; }
+        public BLOCK_TYPE BlockType { get; set; }
+
+        public Block()
+        {
+            IsEmpty = true;
+            BlockType = BLOCK_TYPE.Floor;
+        }
+
+        public Block(BLOCK_TYPE blockType)
+        {
+            BlockType = blockType;
+            IsEmpty = true;
+        }
+
+        public Block(char blockType)
+        {
+            BlockType = (BLOCK_TYPE) blockType;
+            IsEmpty = true;
+        }
+    }
+    public enum BLOCK_TYPE { Floor = ' ', Wall = '#', Goal = '.'}
+}
---
+++ Sokoban/SokobanPC/Level.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SokobanPC
+{
+    class Level
+    {
+        private Block[,] Map;
+        private string exampleLevel = @"  ####
+### @#
+#    #
+# .#.###
+# $    #
+##*#*# #
+# $    #
+#   ####
+#####";
+
+        public Level()
+        {
+            newEmptyLevel(1, 1);
+        }
+
+        public void newEmptyLevel(int sizeX, int sizeY)
+        {
+            Map = new Block[sizeX, sizeY];
+        }
+        public void parseLevel(string _txt)
+        {
+            int numLines = _txt.Split('\n').Length;
+            string line = null;
+
+            StringReader strReader = new StringReader(_txt);
+            line = strReader.ReadLine();
+            int charsInRow = line.Length;
+            Map = new Block[numLines, charsInRow];
+            //to be continue
+        }
+
+        public BLOCK_TYPE getType(int indexX, int indexY)
+        {
+            return Map[indexX, indexY].BlockType;
+        }
+        public bool isEmpty(int indexX, int indexY)
+        {
+            return Map[indexX, indexY].IsEmpty;
+        }
+    }
+}
